447a006f6263616fd9642be2f8f06d2ed4a66aa8
sahar bibas <bibas>
2022-03-25 14:43:19 +0100

    commit 16

--- shortage/decorators.py
+++ shortage/decorators.py
@@ -1,19 +1,27 @@
-from django.shortcuts import redirect
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth.models import User
 
 def allowed_users(allowed_roles=[]):
     def decorator(view_func):
         def wrapper_func (request, *args, **kwargs):
-            group=None 
-            #Traitement SSO here
-            print(request.user)
-            if request.user.groups.exists():#test if user exists in groups
-                # print(request.user)
-                group=request.user.groups.all()[0].name#rutern name of user
-                if group in allowed_roles:
-                    return view_func (request, *args, **kwargs)
+            message=''
+            username='mariem'
+            user=User.objects.all().filter(username=username).first()
+            if user:
+                if user.is_active:
+                    #Traitement SSO here  
+                    if user.groups.exists():#test if user exists in groups
+                        group=user.groups.all()[0].name
+                        if  group and (group in allowed_roles):
+                            return view_func (request, *args, **kwargs)
+                    else:
+                            message='not allowed to acces !'
+                            return render(request,'app/error.html',{'username':username,'message':message})
                 else:
-                    return HttpResponse('you are not autrozied to view page')
+                    message='is not active'
+                    return render(request,'app/error.html',{'username':username,'message':message})
+            else:  
+                message='not found'
+                return render(request,'app/error.html',{'username':username,'message':message})
         return wrapper_func
     return decorator
-
